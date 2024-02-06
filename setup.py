604ad0f4e00404af612b29bbe58d9d6adb01d9f4
arunoruto <21687187+arunoruto@users.noreply.github.com>
2024-02-06 13:25:49 +0100

    Update configs

--- setup.py
+++ setup.py
@@ -5,21 +5,24 @@ from setuptools import setup, find_packages
 try:
     import pycparser as dummy
 except ImportError:
-    print('##############################################\n'
-          '!        ERROR! Caught import error.         !\n'
-          '!      Please, install pycparser first:      !\n'
-          '!                                            !\n'
-          '!            pip install pycparser           !\n'
-          '!                                            !\n'
-          '##############################################\n\n'
-          'For details, see '
-          'https://github.com/pypa/setuptools/issues/391#issuecomment-202919511'
-          '\n\n')
+    print(
+        "##############################################\n"
+        "!        ERROR! Caught import error.         !\n"
+        "!      Please, install pycparser first:      !\n"
+        "!                                            !\n"
+        "!            pip install pycparser           !\n"
+        "!                                            !\n"
+        "##############################################\n\n"
+        "For details, see "
+        "https://github.com/pypa/setuptools/issues/391#issuecomment-202919511"
+        "\n\n"
+    )
 
-setup(name='pywigxjpf',
-      version='1.11',
-      description='Wigner Symbols',
-      long_description="""
+setup(
+    name="pywigxjpf",
+    version="1.11",
+    description="Wigner Symbols",
+    long_description="""
 WIGXJPF evaluates Wigner 3j, 6j and 9j symbols
 accurately using prime factorisation and multi-word integer arithmetic.
 
@@ -69,13 +72,13 @@ are also provided::
     wig6jj_array([jj1,jj2,jj3, jj4,jj5,jj6])
     wig9jj_array([jj1,jj2,jj3, jj4,jj5,jj6, jj7,jj8,jj9])
 """,
-      long_description_content_type='text/x-rst',
-      license = "GPLv3",
-      author='C. Forssen and H. T. Johansson',
-      url='http://fy.chalmers.se/subatom/wigxjpf',
-      packages=['pywigxjpf'],
-      # package_data={'pywigxjpf': ['../lib/libwigxjpf_shared.*']},
-      setup_requires=['numpy',"cffi>=1.0.0"],
-      cffi_modules=["pywigxjpf/pywigxjpf_ffi_builder.py:ffibuilder"],
-      install_requires=['numpy',"cffi>=1.0.0"],
-     )
+    long_description_content_type="text/x-rst",
+    license="GPLv3",
+    author="C. Forssen and H. T. Johansson",
+    url="http://fy.chalmers.se/subatom/wigxjpf",
+    packages=["pywigxjpf"],
+    # package_data={'pywigxjpf': ['../lib/libwigxjpf_shared.*']},
+    setup_requires=["numpy", "cffi>=1.0.0"],
+    cffi_modules=["pywigxjpf/pywigxjpf_ffi_builder.py:ffibuilder"],
+    install_requires=["numpy", "cffi>=1.0.0"],
+)
